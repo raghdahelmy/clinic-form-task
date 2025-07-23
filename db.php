002d61de0c1577fb081b4fb3ed9aa77843087809
Raghda Helmy <raghda.helmy82@gmail.com>
2025-07-24 00:36:46 +0300

    create railway database

--- db.php
+++ db.php
@@ -2,10 +2,11 @@
 $host = "mysql-vefu.railway.internal";
 $dbname = "railway";
 $username = "root";
-$password = "fSjSEFWDiAcNiSpdLwATXuUdaSJKNzvDO";
+$password = "SjSEFWDiAcNiSpdLwATXuUdaSJKNzvDO";
+$port = '3306';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
     // $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Database connection failed.";
